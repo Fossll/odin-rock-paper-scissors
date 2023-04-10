e951ac5877488ada37a1d5272a9bf82a2273b318
Fossll <14176072+Fossll@users.noreply.github.com>
2023-04-10 12:20:55 +0100

    add playRound function, add win / loss messages

--- javascript.js
+++ javascript.js
@@ -1,11 +1,37 @@
 console.log('Hello World!');
 
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 
 }
 
-const randomChoice = getComputerChoice();
-console.log(randomChoice);
+let computerSelection = getComputerChoice();
+//console.log(computerSelection);
+
+let playerSelection = prompt('Enter Input: ').toLowerCase();
+//console.log(playerSelection);
+
+
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    console.log('Player Selection', playerSelection);
+    console.log('Computer Selection', computerSelection);
+
+    if (playerSelection === computerSelection) {
+        console.log('It\'s a draw');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'paper' && computerSelection === 'rock' ||
+        playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('You Win!')
+
+    } else {
+        console.log('Computer Wins')
+    }
+}
+
+
+console.log(playRound(playerSelection));
+
